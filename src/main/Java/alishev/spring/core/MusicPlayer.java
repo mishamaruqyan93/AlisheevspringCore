3bc35a9db0fd2a50b6fce04802f6e2f2d6f7d54a
SmartS <marukyan93@inbox.ru>
2022-10-05 12:19:08 +0300

    Spring Core lesson3 Внедрение зависимостей. Dependency Injection (DI).Часть 2.

--- src/main/Java/alishev/spring/core/MusicPlayer.java
+++ src/main/Java/alishev/spring/core/MusicPlayer.java
@@ -2,13 +2,37 @@ package alishev.spring.core;
 
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private int volume;
 
     //IoC
-    public MusicPlayer(Music music) {
+    //kaxvacutyun enq nerarkum konstruktori mijocov
+//    public MusicPlayer(Music music) {
+//        this.music = music;
+//    }
+//    public MusicPlayer(){}
+    //kaxvacutyun enq nerarkum Set methodi mijocov
+    public void setMusic(Music music) {
         this.music = music;
     }
 
     public void playMusic() {
-        System.out.println("Playing" + music.getSong());
+        System.out.println("Playing :" + " " + music.getSong());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
     }
 }
--- src/main/Java/alishev/spring/core/TestSpring.java
+++ src/main/Java/alishev/spring/core/TestSpring.java
@@ -7,8 +7,11 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
+//        Music music = context.getBean("musicBean", Music.class);
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
+        System.out.println(musicPlayer.getName() + " " + musicPlayer.getVolume());
         context.close();
     }
 }
