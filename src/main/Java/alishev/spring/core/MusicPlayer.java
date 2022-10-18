51b61586925c744d0fd1378d858a589ec49fb286
SmartS <marukyan93@inbox.ru>
2022-10-18 19:43:54 +0300

    Аннотация @Autowired. Внедрение зависимостей (Dependency Injection).

--- src/main/Java/alishev/spring/core/MusicPlayer.java
+++ src/main/Java/alishev/spring/core/MusicPlayer.java
@@ -1,41 +1,20 @@
 package alishev.spring.core;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-
+@Component
 public class MusicPlayer {
-    private Music music;
-    private String name;
-    private int volume;
+    private ClassicalMusic classicalMusic;
+    private RockMusic rockMusic;
 
-//    IoC
-//    kaxvacutyun enq nerarkum konstruktori mijocov
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
-//    public MusicPlayer(){}
-    //kaxvacutyun enq nerarkum Set methodi mijocov
-    public void setMusic(Music music) {
-        this.music = music;
+    @Autowired
+    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
+        this.classicalMusic = classicalMusic;
+        this.rockMusic = rockMusic;
     }
 
-    public void playMusic() {
-        System.out.println("Playing :" + " " + music.getSong());
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
+    public String playMusic() {
+        return "Playing: " + classicalMusic.getSong() + " " + "Playing: " + rockMusic.getSong();
     }
 }
--- src/main/Java/alishev/spring/core/TestSpring.java
+++ src/main/Java/alishev/spring/core/TestSpring.java
@@ -7,18 +7,7 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
-        Music music = context.getBean("rockMusic", Music.class);
-
-        MusicPlayer musicPlayer = new MusicPlayer(music);
-
-        musicPlayer.playMusic();
-
-        Music music2 = context.getBean("classicalMusic", Music.class);
-
-        MusicPlayer classicalMusicPlayer = new MusicPlayer(music2);
-
-        classicalMusicPlayer.playMusic();
-
-        context.close();
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer.toString());
     }
 }
